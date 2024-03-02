40cc87830e444f3a7766c0af8f2994ecf6a90809
fedeee1 <federico.martinez@vasscompany.com>
2024-03-02 20:12:31 +0100

    Class and functions to get location created

--- app/src/main/java/com/example/gpsapp/MainActivity.kt
+++ app/src/main/java/com/example/gpsapp/MainActivity.kt
@@ -2,16 +2,32 @@ package com.example.gpsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import com.example.gpsapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val locationService: LocationService = LocationService()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        getCurrentLocation()
+        binding.btnRefreshLocation.setOnClickListener {
+            getCurrentLocation()
+        }
+    }
+
+    private fun getCurrentLocation(){
+        lifecycleScope.launch {
+            val result = locationService.getLocation(this@MainActivity)
+            if (result != null) {
+                binding.txtLocation.text = "${result.latitude} ${result.longitude}"
+            }
+        }
     }
 }
